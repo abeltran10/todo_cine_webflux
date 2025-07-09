5cf9437fd5d5e747dcd85c0dccc004d74698b50e
abeltran10 <you@example.com>
2025-07-09 22:21:00 +0200

    Initial commit

---
+++ src/main/java/com/todocine_webflux/TodocineWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.todocine_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodocineWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodocineWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todocine_webflux/TodocineWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocine_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodocineWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
